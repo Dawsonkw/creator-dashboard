8f473313da4a0577228f03cc91c7f903612efea2
Dawsonkw <dawsonkw@gmail.com>
2026-04-23 17:12:49 -0600

    initial dashboard push

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl text-blue-700'>Creator Dashboard</h1>
+      <p>A dashboard for twitch stats</p>
+    </>
+  )
+}
+
+export default App
